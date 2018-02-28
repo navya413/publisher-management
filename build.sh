d2682e34a12aa9827e3bd9e6d05b7692ab32b39d
sindhujakoduru <sindhuja.koduru@iiitb.org>
2018-02-28 14:15:01 +0530

    added build script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+echo $JOVEO_ENV
+## cleanup
+docker run --privileged --rm -v `pwd`:/src alpine sh -c "rm -rf /src/node_modules /src/dist"
+
+# Create builder image
+docker build -t pubmato-builder -f .docker/Dockerfile-builder .
+if [ $? -ne 0 ];then
+    exit 1
+fi
+
+# Run the builder image
+docker run -e JOVEO_ENV=$JOVEO_ENV --rm -v `pwd`:/src pubmato-builder:latest
+if [ $? -ne 0 ];then
+    exit 2
+fi
+
+# Build the deployable image
+docker build -t joveo/pubmato -f .docker/Dockerfile .
+if [ $? -ne 0 ];then
+    exit 3
+fi
+
+# remove the builder image
+docker rmi -f pubmato-builder
+if [ $? -ne 0 ];then
+    exit 4
+fi
+
+## cleanup
+docker run --privileged --rm -v `pwd`:/src alpine sh -c "rm -rf /src/node_modules /src/dist"
