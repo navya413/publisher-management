1d4f1505526870bec0a875da61f7d05e7f308b8b
Krishna Agrawal <krishna@joveo.com>
2018-02-28 18:14:00 +0530

    type error resolved

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  authApi: 'http://54.144.227.5/api/',
-  adminApi: 'http://54.144.227.5/admin/api/',
-  feedsApi: 'http://54.144.227.5/'
+  authApi: 'http://54.144.227.5:11001/api/',
+  adminApi: 'http://54.144.227.5:11001/admin/api/',
+  feedsApi: 'http://54.144.227.5:8001/'
 };
