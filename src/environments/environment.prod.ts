730cfe0efb4584045d5b614aaaeb26cd9df69e69
Krishna Agrawal <krishna@joveo.com>
2018-04-09 21:10:25 +0530

    api endpoint changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   authApi: 'http://mojo.prod.joveo.com/api/',
-  adminApi: 'http://mojo.prod.joveo.com/admin/api/',
+  adminApi: 'http://mojo.prod.joveo.com/api/staging',
   feedsApi: 'http://54.144.227.5:8080/'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   authApi: 'http://stag.joveo.com/api/',
-  adminApi: 'http://stag.joveo.com/admin/api/',
+  adminApi: 'http://stag.joveo.com/api/admin/',
   feedsApi: 'http://54.144.227.5:8080/'
 };
