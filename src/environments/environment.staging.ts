72e27573760ef394f5c311d11d637989f5b31dc0
Krishna Agrawal <krishna@joveo.com>
2018-04-10 11:47:19 +0530

    environment changes

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  authApi: 'http://aragorn-api.staging.joveo.com/api/',
-  adminApi: 'http://aragorn-api.staging.joveo.com/admin/api/',
+  authApi: 'http://stag.joveo.com/api/',
+  adminApi: 'http://stag.joveo.com/api/admin/',
   feedsApi: 'http://54.144.227.5:8080/',
   newStatsApi: 'http://legolas.joveo.com:8080/gandalf/stats/'
 };
