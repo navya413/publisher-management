910f5db5b1d583366c564cd2bd32b7edb7c635fc
Rahul Chaki <rahul.chaki@gmail.com>
2018-05-12 16:27:24 +0530

    New Gandalf Api host change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   authApi: '/api/',
   adminApi: '/api/admin/',
   feedsApi: 'http://54.144.227.5:8080/',
-  newStatsApi: 'http://legolas.joveo.com:8080/gandalf/stats/'
+  newStatsApi: 'http://54.146.60.243:8080/gandalf/app/'
 };
