680aa5a2ba8513bd5048b27a046cf654889b8888
Krishna Agrawal <krishna@joveo.com>
2019-01-22 13:10:52 +0530

    Revert "feat(trackingUrl): add tracking URL to schema mapping"
    This reverts commit 0fd124270a5b8b718be34f5da25829d3f9ad8078.

--- src/app/pub-management/pub-management.component.ts
+++ src/app/pub-management/pub-management.component.ts
@@ -221,7 +221,7 @@ export class PublisherListComponent implements OnInit {
         this.closeEditor();
         if (type === 'feedFileType') {
           row['feedFileInfo']['fileType'] = value;
-          if (!row['feedFileInfo']['toggleTimestamp']) {
+          if (!row['feedFileInfo']['toggleTimestamp']){
             row['feedFileInfo']['toggleTimestamp'] = new Date();
             row['feedFileInfo']['timeElapsed'] = 1000;
           }
@@ -323,8 +323,7 @@ export class PublisherSchemaDialog implements OnInit {
       publishedDate: '',
       advertiser: '',
       cpc: '',
-      cpa: '',
-      trackingUrl: ''
+      cpa: ''
     };
     this.headerSchema = {
       publisherName: '',
@@ -640,7 +639,7 @@ export class PublisherAddDialog implements OnInit, OnDestroy {
   public creationForm: FormGroup;
   ftpConfigSubscription$;
 
-  flatBidPublisher = false;
+  flatBidPublisher: boolean = false;
 
   constructor(
     public dialogRef: MatDialogRef<PublisherAddDialog>,
