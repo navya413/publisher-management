27a6d5360468fad6caee6340af0c5023e8a6872a
Naveen <naveeng@joveo.com>
2019-08-20 16:05:48 +0530

    Added stats compare options.

--- src/app/v2/publisher-stats/publisher-stats.component.ts
+++ src/app/v2/publisher-stats/publisher-stats.component.ts
@@ -7,6 +7,8 @@ import { ApiService } from '../../services/api.service';
 import { ActivatedRoute } from '@angular/router';
 import { V2Service } from '../v2.service';
 import { BreadcrumbSegment } from '../../core/components/breadcrumb/breadcrumb.model';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-publisher-stats',
@@ -34,15 +36,67 @@ export class PublisherStatsComponent implements OnInit {
   selectedView:string = "Stats";
   viewOptions :string[]  = VIEW_OPTIONS
 
-  compareOptions = {
-    "joveo_clicks" : ['Pub Rec Clicks', 'CM Clicks'],
-    "pubrec_clicks" : ['Joveo Clicks', 'CM Clicks'],
-    "cm_clicks": ['Joveo Clicks', 'Pub Rec Clicks'],
-    "ch_clicks" : [],
-    "web_spend":[]
-  }
 
-  constructor(private apiSerivce : ApiService,private activatedRoute : ActivatedRoute,private v2Service : V2Service) { }
+  clicks_compare_config = [
+    {
+      "label" : "Joveo Clicks",
+      "dataKey" : "joveo",
+      "toolTip" : "",
+      "entities" : []
+    },
+    {
+      "label" : "Pub Rec Clicks",
+      "dataKey" : "pub",
+      "toolTip" : "",
+      "entities" : []
+    },{
+      "label" : "CM Clicks",
+      "dataKey" : "cm",
+      "toolTip" : "",
+      "entities" : []
+    },{
+      "label" : "CH Clicks",
+      "dataKey" : "ch",
+      "toolTip" : "",
+      "entities" : []
+    }
+  ]
+
+
+  spend_compare_config = [
+    {
+      "label" : "Joveo Spend",
+      "dataKey" : "joveo",
+      "toolTip" : "",
+      "entities" : []
+    },
+    {
+      "label" : "Pub Rec Spend",
+      "dataKey" : "pub",
+      "toolTip" : "",
+      "entities" : []
+    },{
+      "label" : "Webscrape Spend",
+      "dataKey" : "pubPortal",
+      "toolTip" : "",
+      "entities" : []
+    },{
+      "label" : "CH Spend",
+      "dataKey" : "ch",
+      "toolTip" : "",
+      "entities" : []
+    }
+  ]
+
+  constructor(private apiSerivce : ApiService,private activatedRoute : ActivatedRoute,private v2Service : V2Service,
+    private domSanitizer: DomSanitizer,
+    iconRegistry: MatIconRegistry) { 
+
+      iconRegistry.addSvgIcon(
+        "multi-drop",
+        this.domSanitizer.bypassSecurityTrustResourceUrl("../../../assets/images/svg-icons/multi-drop.svg")
+      );
+    }
 
   ngOnInit() {
     this.agencyId = this.activatedRoute.snapshot.params.agencyId;
@@ -54,6 +108,27 @@ export class PublisherStatsComponent implements OnInit {
     this.getStats()
   }
 
+  comparemenu;
+  showClicksCompareEntities(entity:any){
+    this.comparemenu = this.clicks_compare_config.filter(it=>{
+      return it.label != entity
+    })
+    this.comparemenu.forEach(item=>{
+      item.entities = [entity,item.label]
+      item.toolTip = entity + " vs " + item.label
+    })
+  }
+
+  showSpendCompareEntities(entity:any){
+    this.comparemenu = this.spend_compare_config.filter(it=>{
+      return it.label != entity
+    })
+    this.comparemenu.forEach(item=>{
+      item.entities = [entity,item.label]
+      item.toolTip = entity + " vs " + item.label
+    })
+  }
+
   buildBreadcrumb() {
     this.breadcrumbSegments = [{ label: "All Agencies", url: ["../../..", "agencies"] }, { label: "Agency : ", subTitle: this.agencyId }];
   }
@@ -62,8 +137,65 @@ export class PublisherStatsComponent implements OnInit {
   }
 
 
-  bodyClassDecider(type, metric) {
+  bodyClassDecider(metric) {
+    if(metric.indexOf("Spend") != -1){
+      if (this.selectedSpendComparemenu && this.selectedSpendComparemenu.entities.indexOf(metric) != -1){
+        return true
+      }
+      return false  
+    }
+    if (this.selectedComparemenu && this.selectedComparemenu.entities.indexOf(metric) != -1){
+      return true
+    }
+    return false
+  }
 
+  compareClicksKey = ""
+  compareSpendKey = ""
+  compareTooltip = ""
+  spendcompareTooltip = ""
+  selectedComparemenu :any;
+  selectedSpendComparemenu :any;
+
+  onCloseCompare(compareType){
+    if(compareType === "clicks"){
+      this.compareClicksKey = ""
+      this.selectedComparemenu = null
+    }
+    if(compareType === "spend"){
+      this.compareSpendKey = ""
+      this.selectedSpendComparemenu = null
+    }
+    this.compareTooltip = ""
+  }
+
+
+  onMenuSelection(parentKey,metric1, menu) {
+    this.selectedComparemenu = menu
+    this.compareClicksKey = "compare_clicks"
+    this.compareTooltip = menu.toolTip;
+    this.totalStatsResp.data.forEach(item => {
+        item["compare_clicks"] = this.getDiscrPerc(item[parentKey][metric1],item[parentKey][menu.dataKey])
+    });
+  }
+
+  onSpendMenuSelection(parentKey,metric1, menu){
+    this.selectedSpendComparemenu = menu
+    this.compareSpendKey = "compare_spend"
+    this.spendcompareTooltip = menu.toolTip;
+    this.totalStatsResp.data.forEach(item => {
+        item["compare_spend"] = this.getDiscrPerc(item[parentKey][metric1],item[parentKey][menu.dataKey])
+    });
+  }
+
+  getDiscrPerc(val1, val2) {
+    if (val1 === 0) {
+      return '--';
+    } else if (val2 === 0) {
+      return 100;
+    } else {
+      return Math.round(((val1 - val2) / val1) * 100 * 100) / 100;
+    }
   }
 
   onDateChange(dateRange) {
